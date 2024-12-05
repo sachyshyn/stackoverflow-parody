a2517104ce322fdc781260c4084c0762e7aebfe6
sachyshyn <alexandr.sachyshyn@gmail.com>
2024-12-05 03:00:30 +0300

    refactor: db config

--- src/config/database/postgres.config.ts
+++ src/config/database/postgres.config.ts
@@ -6,17 +6,40 @@ import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 export class PostgresConfigService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
 
+  get #host() {
+    return this.configService.get<string>('DATABASE_HOST') ?? 'localhost';
+  }
+
+  get #port() {
+    return Number(this.configService.get<string>('DATABASE_PORT')) ?? 3000;
+  }
+
+  get #username() {
+    return this.configService.get<string>('DATABASE_USER');
+  }
+
+  get #password() {
+    return this.configService.get<string>('DATABASE_PASSWORD');
+  }
+
+  get #database() {
+    return this.configService.get<string>('DATABASE_NAME');
+  }
+
+  get #shallSynchronize() {
+    return this.configService.get<string>('NODE_ENV') !== 'production';
+  }
+
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      host: this.configService.get<string>('DATABASE_HOST') ?? 'localhost',
-      port:
-        parseInt(this.configService.get<string>('DATABASE_PORT'), 10) ?? 3000,
-      username: this.configService.get<string>('DATABASE_USER'),
-      password: this.configService.get<string>('DATABASE_PASSWORD'),
-      database: this.configService.get<string>('DATABASE_NAME'),
+      host: this.#host,
+      port: this.#port,
+      username: this.#username,
+      password: this.#password,
+      database: this.#database,
       entities: [],
-      synchronize: this.configService.get<string>('NODE_ENV') !== 'production',
+      synchronize: this.#shallSynchronize,
     };
   }
 }
