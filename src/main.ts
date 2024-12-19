6bb00d52cbce3b4d7c2217138a1544ded6a35c84
Alexandr Sachyshyn <sachyshyn@MacBook-Pro-Alexandr.local>
2024-12-19 21:00:30 +0300

    feat: added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 (async () => {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Stack overflow parody (SOP) BE')
+    .setDescription('SOP BE description')
+    .setVersion('1.0')
+    .addTag('SOP')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 })();
