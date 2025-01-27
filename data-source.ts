f467c902802e6fdfe028195b5739324306736d65
Alexandr Sachyshyn <sachyshyn@MacBook-Pro-Alexandr.local>
2025-01-27 15:16:11 +0300

    feat: added data source

---
+++ data-source.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+import { ConfigService } from '@nestjs/config';
+
+config();
+
+const configService = new ConfigService();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: configService.get('DATABASE_HOST'),
+  port: configService.get('DATABASE_PORT'),
+  username: configService.get('DATABASE_USER'),
+  password: configService.get('DATABASE_PASSWORD'),
+  database: configService.get('DATABASE_NAME'),
+  synchronize: false,
+  entities: ['"src/modules/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/**'],
+  subscribers: ['src/subscribers/*.ts'],
+});
