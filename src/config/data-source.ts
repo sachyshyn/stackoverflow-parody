809b7d12a75b0a147cdfaf573613f426f930c39c
Alexandr Sachyshyn <sachyshyn@MacBook-Pro-Alexandr.local>
2025-02-01 00:55:50 +0300

    fix: build main

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -2,7 +2,7 @@ import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
 
 config();
-console.log(process.env.DATABASE_PORT);
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DATABASE_HOST,
